9f305cd0aa469437428622a70169f4896054e1b1
Alex1010102 <vasilenkoav1@mail.ru>
2023-03-28 20:45:59 +0500

    23.03.2023 cycles

--- src/HomeWork_3/Main.java
+++ src/HomeWork_3/Main.java
@@ -7,19 +7,19 @@ public class Main {
 
         // Задача 1.
 
+        System.out.println("1) Вывести на экран прямоугольник");
         int m = 2;
         int n = 4;
-
         for (int i = 0; i < m; i++) {
             System.out.println();
             for (int g = 0; g < n; g++) {
                 System.out.print("8");
-                ;
             }
         }
-        System.out.println();
 
         // Задача 2.
+        System.out.println("");
+        System.out.println("2)Вывести на экран прямоугольный треугольник из восьмерок");
         for (int t = 0; t < 10; t++) {
             System.out.println();
             for (int r = 0; r <= t; r++) {
@@ -30,6 +30,8 @@ public class Main {
 
         // Задача 3.
 
+        System.out.print("3)Ввести с клавиатуры три числа");
+        System.out.println();
         Scanner number = new Scanner(System.in);
         int p1 = number.nextInt();
         int p2 = number.nextInt();
@@ -41,37 +43,44 @@ public class Main {
             System.out.print(p2);
         } else
             System.out.print(p3);
+        System.out.println("");
 
         // Задача 4.
+
+        System.out.println("4)Вывести на экран таблицу умножения 10х10");
 
         int q = 1;
         int w = 1;
         while (q <= 10) {
-            System.out.print( q + " ");
+            System.out.print(q + " ");
             while (w <= 9) {
                 w++;
-                System.out.print( q * w + " ");
+                System.out.print(q * w + " ");
             }
             q++;
             w = 1;
             System.out.println(" ");
-
+        }
 
             // Задача 5.
-            Scanner na = new Scanner(System.in);
-            String name = na.nextLine();
+            System.out.println("5)Ввести с клавиатуры строку name");
+            Scanner nam = new Scanner(System.in);
+            String name = nam.nextLine();
 
-            int Day = na.nextInt();
-            int Month = na.nextInt();
-            int Year = na.nextInt();
+            int Day = nam.nextInt();
+            int Month = nam.nextInt();
+            int Year = nam.nextInt();
 
             System.out.printf("Меня зовут %s", name);
-            System.out.println(" Я родился " + Day + "." + Month + "." + Year + ".");
+            System.out.println();
+            System.out.println("Я родился " + Day + "." + Month + "." + Year + "");
+
+
 
         }
 
     }
-}
+
 
 
 
