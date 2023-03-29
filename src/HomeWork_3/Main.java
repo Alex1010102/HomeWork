d5cf193b977aa8b8bacae8765bf22f7d2d03b918
Alex1010102 <vasilenkoav1@mail.ru>
2023-03-29 15:39:00 +0500

    28.03.2023

--- src/HomeWork_3/Main.java
+++ src/HomeWork_3/Main.java
@@ -6,19 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача 1.
+        System.out.println("1) Введите стороны прямоугольника m и n: ");
+        Scanner Rectangle = new Scanner(System.in);
+        int m = Rectangle.nextInt();
+        Scanner Rectangle1 = new Scanner(System.in);
+        int n = Rectangle1.nextInt();
 
-        System.out.println("1) Вывести на экран прямоугольник");
-        int m = 2;
-        int n = 4;
         for (int i = 0; i < m; i++) {
-            System.out.println();
             for (int g = 0; g < n; g++) {
                 System.out.print("8");
             }
+            System.out.println();
         }
 
+
         // Задача 2.
-        System.out.println("");
         System.out.println("2)Вывести на экран прямоугольный треугольник из восьмерок");
         for (int t = 0; t < 10; t++) {
             System.out.println();
@@ -28,6 +30,7 @@ public class Main {
         }
         System.out.println();
 
+
         // Задача 3.
 
         System.out.print("3)Ввести с клавиатуры три числа");
@@ -36,17 +39,26 @@ public class Main {
         int p1 = number.nextInt();
         int p2 = number.nextInt();
         int p3 = number.nextInt();
+        System.out.print("Cреднее из них: ");
 
-        if (p1 < p2 && p1 > p3) {
-            System.out.print(p1);
-        } else if (p2 > p1 && p2 < p3) {
-            System.out.print(p2);
-        } else
-            System.out.print(p3);
-        System.out.println("");
+        if (p1 == p2 && p2 == p3) {
+            System.out.println(p1);
+        } else if (((p1 == p2 && p2 != p3) || (p1 == p3 && p1 != p2) || (p1 == p3 && p1 != p2))) {
+            System.out.println(p1);
+        } else if (p1 == p3 && p1 != p2) {
+            System.out.println(p1);
+        } else if (p2 == p3 && p2 != p1) {
+            System.out.println(p2);
+        } else if ((p1 > p2 && p2 > p3) || (p3 > p2 && p2 > p1)) {
+            System.out.println(p2);
+        } else if ((p1 > p3 && p3 > p2) || (p2 > p3 && p3 > p1)) {
+            System.out.println(p3);
+        } else if ((p3 > p1 && p1 > p2) || (p2 > p1 && p1 > p3)) {
+            System.out.println(p1);
+        }
 
         // Задача 4.
-
+        System.out.println();
         System.out.println("4)Вывести на экран таблицу умножения 10х10");
 
         int q = 1;
@@ -61,12 +73,11 @@ public class Main {
             w = 1;
             System.out.println(" ");
         }
-
             // Задача 5.
-            System.out.println("5)Ввести с клавиатуры строку name");
+            System.out.println("5)Введите имя");
             Scanner nam = new Scanner(System.in);
             String name = nam.nextLine();
-
+            System.out.println("Введите дату рождения (день, месяц, год): ");
             int Day = nam.nextInt();
             int Month = nam.nextInt();
             int Year = nam.nextInt();
@@ -74,14 +85,18 @@ public class Main {
             System.out.printf("Меня зовут %s", name);
             System.out.println();
             System.out.println("Я родился " + Day + "." + Month + "." + Year + "");
-
-
-
-
-
         }
 
-    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
