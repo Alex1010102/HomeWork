74806ebd6f79a6f2e470ba2f9002981f1a26411f
Alex1010102 <vasilenkoav1@mail.ru>
2023-04-13 12:33:30 +0500

    10.04

---
+++ src/Game/Game.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class Game {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.playGame();
+    }
+}
---
+++ src/Game/TicTacToe.java
@@ -0,0 +1,156 @@
+package Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    Scanner scanner;
+    char [][] table;
+    Random random;
+
+    public TicTacToe() {
+        scanner = new Scanner(System.in);
+        random = new Random();
+        table = new char[5][5];
+    }
+
+    public void playGame() {
+        System.out.println("GAME IS STARTED!");
+        initTable();
+        printTable();
+
+        while (true) {
+            turnHuman();
+            if (checkWin('x')) {
+                printTable();
+                System.out.println("YOU WON!!!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("DRAW");
+            }
+            turnAI();
+            printTable();
+            if (checkWin('o')) {
+                printTable();
+                System.out.println("WON THE COMPUTER!");
+                break;
+            }
+        }
+    }
+    public void initTable() {
+        for (int x = 0; x < table.length; x++) {
+            for (int y = 0; y < table[x].length; y++) {
+                table [x][y] = '.';
+            }
+        }
+    }
+    public void printTable() {
+        for (int x = 0; x < table.length; x++) {
+            for (int y = 0; y < table[x].length; y++) {
+                System.out.print(table [x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public void turnHuman() {
+        int x, y;
+
+        do {
+        System.out.println("Enter x and y from [0...4]");
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+
+           } while (!isCellValid(x, y));
+            table [x][y] = 'x';
+        }
+    public boolean isCellValid (int x, int y) {
+        if (x < 0 || x > 4 || y < 0 || y > 4) {
+            return false;
+        }
+        return table [x][y] == '.';
+    }
+    public boolean checkWin(char symbol) {
+        // horizontal
+        if (table[0][0] == symbol && table[0][1] == symbol && table[0][2] == symbol && table[0][3] == symbol)
+            return true;
+        if (table[0][1] == symbol && table[0][2] == symbol && table[0][3] == symbol && table[0][4] == symbol)
+            return true;
+        if (table[1][0] == symbol && table[1][1] == symbol && table[1][2] == symbol && table[1][3] == symbol)
+            return true;
+        if (table[1][1] == symbol && table[1][2] == symbol && table[1][3] == symbol && table[1][4] == symbol)
+            return true;
+        if (table[2][0] == symbol && table[2][1] == symbol && table[2][2] == symbol && table[2][3] == symbol)
+            return true;
+        if (table[2][1] == symbol && table[2][2] == symbol && table[2][3] == symbol && table[2][4] == symbol)
+            return true;
+        if (table[3][0] == symbol && table[3][1] == symbol && table[3][2] == symbol && table[3][3] == symbol)
+            return true;
+        if (table[3][1] == symbol && table[3][2] == symbol && table[3][3] == symbol && table[3][4] == symbol)
+            return true;
+        if (table[4][0] == symbol && table[4][1] == symbol && table[4][2] == symbol && table[4][3] == symbol)
+            return true;
+        if (table[4][1] == symbol && table[4][2] == symbol && table[4][3] == symbol && table[4][4] == symbol)
+            return true;
+
+        // vertical
+        if (table[0][0] == symbol && table[1][0] == symbol && table[2][0] == symbol && table[3][0] == symbol)
+            return true;
+        if (table[1][0] == symbol && table[2][0] == symbol && table[3][0] == symbol && table[4][0] == symbol)
+            return true;
+        if (table[0][1] == symbol && table[1][1] == symbol && table[2][1] == symbol && table[3][1] == symbol)
+            return true;
+        if (table[1][1] == symbol && table[2][1] == symbol && table[3][1] == symbol && table[4][1] == symbol)
+            return true;
+        if (table[0][2] == symbol && table[1][2] == symbol && table[2][2] == symbol && table[3][2] == symbol)
+            return true;
+        if (table[1][2] == symbol && table[2][2] == symbol && table[3][2] == symbol && table[4][2] == symbol)
+            return true;
+        if (table[0][3] == symbol && table[1][3] == symbol && table[2][3] == symbol && table[3][3] == symbol)
+            return true;
+        if (table[1][3] == symbol && table[2][3] == symbol && table[3][3] == symbol && table[4][3] == symbol)
+            return true;
+        if (table[0][4] == symbol && table[1][4] == symbol && table[2][4] == symbol && table[3][4] == symbol)
+            return true;
+        if (table[1][4] == symbol && table[2][4] == symbol && table[3][4] == symbol && table[4][4] == symbol)
+            return true;
+
+        // diagonal
+        if (table[0][0] == symbol && table[1][1] == symbol && table[2][2] == symbol && table[3][3] == symbol)
+            return true;
+        if (table[1][1] == symbol && table[2][2] == symbol && table[3][3] == symbol && table[4][4] == symbol)
+            return true;
+        if (table[0][4] == symbol && table[1][3] == symbol && table[2][2] == symbol && table[3][1] == symbol)
+            return true;
+        if (table[1][3] == symbol && table[2][2] == symbol && table[3][1] == symbol && table[4][0] == symbol)
+            return true;
+        if (table[0][1] == symbol && table[1][2] == symbol && table[2][3] == symbol && table[3][4] == symbol)
+            return true;
+        if (table[1][0] == symbol && table[2][1] == symbol && table[3][2] == symbol && table[4][3] == symbol)
+            return true;
+        if (table[0][3] == symbol && table[1][2] == symbol && table[2][1] == symbol && table[3][0] == symbol)
+            return true;
+        if (table[1][4] == symbol && table[2][3] == symbol && table[3][2] == symbol && table[4][1] == symbol)
+            return true;
+
+        return false;
+    }
+    public boolean isTableFull () {
+        for (int x = 0; x < table.length; x++) {
+            for (int y = 0; y < table[x].length; y++) {
+                if (table[x][y] == '.') return false;
+            }
+        }
+        return true;
+
+    }
+    public void turnAI() {
+        int x, y;
+        do {
+            x = random.nextInt(0,4);
+            y = random.nextInt(0,4);
+        } while (!isCellValid(x, y));
+        table [x][y] = 'o';
+
+    }
+}
